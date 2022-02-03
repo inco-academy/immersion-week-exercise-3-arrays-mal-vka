701bbcbd657807f5856c543020411be6dc5f6142
mal-vka <malwina.g@gmial.com>
2022-02-03 21:24:34 +0100

    replace toBe with toStrictEqual in test

--- src/4_replace_array.test.js
+++ src/4_replace_array.test.js
@@ -1,7 +1,7 @@
 const replaceArray = require("./4_replace_array");
 
 test("Replace Array (1)", () => {
-  expect(replaceArray(["banana", "suit", "hammer"])).toBe([
+  expect(replaceArray(["banana", "suit", "hammer"])).toStrictEqual([
     "banana",
     "suit",
     "HAMMER",
@@ -9,9 +9,9 @@ test("Replace Array (1)", () => {
 });
 
 test("Replace Array (2)", () => {
-  expect(replaceArray(["Hello", "there"])).toBe(["HELLO", "there"]);
+  expect(replaceArray(["Hello", "there"])).toStrictEqual(["HELLO", "there"]);
 });
 
 test("Replace Array (3)", () => {
-  expect(replaceArray(["Hey!", "hey..."])).toBe(["HEY!", "HEY..."]);
+  expect(replaceArray(["Hey!", "hey..."])).toStrictEqual(["HEY!", "HEY..."]);
 });
